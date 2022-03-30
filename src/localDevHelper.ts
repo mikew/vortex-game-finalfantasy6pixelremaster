c023db1de657e422c1e417861b009b7f979f5eb9
Mike Wyatt <wyatt.mike@gmail.com>
2022-03-30 01:51:57 -0300

    need to figure out a better solution local dev

--- src/localDevHelper.ts
+++
@@ -1,5 +0,0 @@
-// This file is to help when developing locally in `AppData/Vortex`. Instead of
-// copying all built files to the root, the build script copies this file as
-// `index.js`.
-// @ts-ignore
-export { default } from './dist'
